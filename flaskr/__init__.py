ec47a1cdea23b8f8bc8653b25041e97861a10019
Caerwyn Sabin-Rees <60520086+Caerwyn13@users.noreply.github.com>
2025-11-21 10:35:00 +0000

    Update __all__ variable and import statements

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,16 +1,19 @@
 # Define the __all__ variable
-__all__ = ["db", "plotting"]
+__all__ = ["auth", "db", "plotting", "querying", "app"]
 
 # Import the submodules
+from . import auth
 from . import db
 from . import plotting
-
+from . import querying
+from . import user
 
 """
 FLASK CODE
 """
 import os
 from flask import Flask
+# from flask_cors import CORS       # Not available on VM
 
 def create_app(test_conf=None):
     # Create and configure the app
@@ -31,13 +34,10 @@ def create_app(test_conf=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-
-    # Simple test page
-    @app.route('/hello')
-    def hello():
-        return 'Hello!'
     
-    from . import db
     db.init_app(app)
-    
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(user.bp)
+
+    #CORS(app)   # Enable CORS
     return app
