274250411c163cff70d0b1f4730dce7c857a25d1
Caerwyn Sabin-Rees <60520086+Caerwyn13@users.noreply.github.com>
2025-12-05 15:22:51 +0000

    Fix accessLog schema and insert departments
    Updated the 'received' column to have a proper TIMESTAMP type and added initial department entries.

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -24,10 +24,33 @@ CREATE TABLE accessLog (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     RFID_key TEXT NOT NULL,
     accessed TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    received CURRENT_TIMESTAMP,
+    received TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     actiontype TEXT NOT NULL,
     is_authorised INTEGER,
     FOREIGN KEY (RFID_key) REFERENCES users(RFID_key)
 );
 
 CREATE INDEX idx_accesslog_rfid ON accessLog(RFID_key);
+
+
+INSERT INTO departments(department_id, department_name) VALUES
+(1, 'Administration'),
+(2, 'Marketing'),
+(3, 'Purchasing'),
+(4, 'Human Resources'),
+(5, 'Shipping'),
+(6, 'IT'),
+(7, 'Public Relations'),
+(8, 'Sales'),
+(9, 'Executive'),
+(10, 'Finance'),
+(11, 'Accounting'),
+(12, 'Treasury'),
+(13, 'Shareholder Services'),
+(14, 'Manufacturing'),
+(15, 'Contracting'),
+(16, 'Operational Security'),
+(17, 'NOC'),
+(18, 'Helpdesk'),
+(19, 'Recruiting');
+
